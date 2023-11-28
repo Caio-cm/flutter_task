e457ed10a7e4a9d2f48430ffd83dd4a9e2030869
Caio Marques <caio.4910@aluno.pr.senac.br>
2023-11-28 16:52:32 -0300

    Inserção de imagem via link nas tarefas, inclusão do texto no botão, modificação da logica da barra de processo

--- lib/main.dart
+++ lib/main.dart
@@ -20,16 +20,13 @@ class MyApp extends StatelessWidget {
         ),
         body: ListView(
           children: [
-            Task('Aprender Flutter kkkkkkkkkkkkkkkkkkkkkkkkkkk caio'),
-            Task('Andar de bike'),
-            Task('Meditar'),
-            Task('Ir para o intervalo'),
-            Task('Ir para o intervalo'),
-            Task('Ir para o intervalo'),
-            Task('Ir para o intervalo'),
-            Task('Ir para o intervalo'),
-            Task('Ir para o intervalo'),
-            Task('Ir para o intervalo'),
+            Task('Aprender Flutter', ''),
+            Task('Andar de bike',
+                'https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg'),
+            Task('Meditar',
+                'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg'),
+            Task('Ler', ''),
+            Task('jogar ', ''),
           ],
         ),
         floatingActionButton: FloatingActionButton(onPressed: () {}),
@@ -40,8 +37,11 @@ class MyApp extends StatelessWidget {
 
 class Task extends StatefulWidget {
   final String nome;
+  final String foto;
+  final int dificuldade;
 
-  const Task(this.nome, {Key? key}) : super(key: key);
+  const Task(this.nome, this.foto, this.dificuldade, {Key? key})
+      : super(key: key);
 
   @override
   State<Task> createState() => _TaskState();
@@ -49,10 +49,9 @@ class Task extends StatefulWidget {
 
 class _TaskState extends State<Task> {
   int nivel = 0;
+
   @override
   Widget build(BuildContext context) {
-
-
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
@@ -71,25 +70,74 @@ class _TaskState extends State<Task> {
                         color: Colors.black26,
                         width: 80,
                         height: 100,
-                      ),
-                      Container(
-                        width: 200,
-                        child: Text(
-                          widget.nome,
-                          style: TextStyle(
-                            fontSize: 24,
-                            overflow: TextOverflow.ellipsis,
-                          ),
+                        child: Image.network(
+                          widget.foto,
+                          fit: BoxFit.cover,
                         ),
                       ),
-                      ElevatedButton(
-                          onPressed: () {
-                            setState((){
-                              nivel++;
-                            });
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: 200,
+                            child: Text(
+                              widget.nome,
+                              style: TextStyle(
+                                fontSize: 24,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Icon(Icons.star,
+                                  size: 15,
+                                  color: (widget.dificuldade >= 1)
+                                      ? Colors.blue
+                                      : Colors.blue[100]),
+                              Icon(Icons.star,
+                                  size: 15,
+                                  color: (widget.dificuldade >= 2)
+                                      ? Colors.blue
+                                      : Colors.blue[100]),
+                              Icon(Icons.star,
+                                  size: 15,
+                                  color: (widget.dificuldade >= 3)
+                                      ? Colors.blue
+                                      : Colors.blue[100]),
+                              Icon(Icons.star,
+                                  size: 15,
+                                  color: (widget.dificuldade >= 4)
+                                      ? Colors.blue
+                                      : Colors.blue[100]),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Container(
+                        height: 52,
+                        width: 82,
+                        child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                nivel++;
+                              });
 
-                            print(nivel);
-                          }, child: Icon(Icons.arrow_drop_up))
+                              print(nivel);
+                            },
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Icon(Icons.arrow_drop_up),
+                                Text(
+                                  'lvl Up',
+                                  style: TextStyle(fontSize: 12),
+                                )
+                              ],
+                            )),
+                      )
                     ],
                   ),
                 ),
@@ -107,10 +155,7 @@ class _TaskState extends State<Task> {
                       ),
                       Text(
                         'Nivel: $nivel',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16
-                        ),
+                        style: TextStyle(color: Colors.white, fontSize: 16),
                       ),
                     ],
                   ),
